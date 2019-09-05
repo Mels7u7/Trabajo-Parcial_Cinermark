f6db9aec1d0faffa108fb9d58d256b627c2cc1c3
porta <porta@10.142.70.248>
2019-09-05 14:56:38 -0500

    creacion de la entidad Especialidad

--- TrabajoParcial/src/main/java/pe/edu/upc/entity/Especialidad.java
+++ TrabajoParcial/src/main/java/pe/edu/upc/entity/Especialidad.java
@@ -2,4 +2,42 @@ package pe.edu.upc.entity;
 
 public class Especialidad {
 
+	private int idEspecialidad;
+	private String nombreEspecialidad;
+	private String certificadoraEspecialidad;
+	
+	public Especialidad() {
+		super();
+	}
+
+	public Especialidad(int idEspecialidad, String nombreEspecialidad, String certificadoraEspecialidad) {
+		super();
+		this.idEspecialidad = idEspecialidad;
+		this.nombreEspecialidad = nombreEspecialidad;
+		this.certificadoraEspecialidad = certificadoraEspecialidad;
+	}
+
+	public int getIdEspecialidad() {
+		return idEspecialidad;
+	}
+
+	public void setIdEspecialidad(int idEspecialidad) {
+		this.idEspecialidad = idEspecialidad;
+	}
+
+	public String getNombreEspecialidad() {
+		return nombreEspecialidad;
+	}
+
+	public void setNombreEspecialidad(String nombreEspecialidad) {
+		this.nombreEspecialidad = nombreEspecialidad;
+	}
+
+	public String getCertificadoraEspecialidad() {
+		return certificadoraEspecialidad;
+	}
+
+	public void setCertificadoraEspecialidad(String certificadoraEspecialidad) {
+		this.certificadoraEspecialidad = certificadoraEspecialidad;
+	}
 }
