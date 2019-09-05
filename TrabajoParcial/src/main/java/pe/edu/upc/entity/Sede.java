9494b297565bf1c1715022cf6082b0a85805e986
porta <porta@10.142.70.248>
2019-09-05 15:21:29 -0500

    implementacion de JPA en la entidad sede

--- TrabajoParcial/src/main/java/pe/edu/upc/entity/Sede.java
+++ TrabajoParcial/src/main/java/pe/edu/upc/entity/Sede.java
@@ -1,8 +1,28 @@
 package pe.edu.upc.entity;
 
-public class Sede {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sede")
+public class Sede implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idSede;
+	
+	@Column(name = "direccionSede", nullable = false, length = 70)
 	private String direccionSede;
 	
 	public Sede() {
