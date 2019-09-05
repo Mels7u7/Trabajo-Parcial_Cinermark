14a0ec4cd6e380680b12825b9cc6d2029af0d3c9
porta <porta@10.142.70.248>
2019-09-05 14:39:30 -0500

    Desarrollo de clase entidad Sede

--- TrabajoParcial/src/main/java/pe/edu/upc/entity/Sede.java
+++ TrabajoParcial/src/main/java/pe/edu/upc/entity/Sede.java
@@ -2,4 +2,32 @@ package pe.edu.upc.entity;
 
 public class Sede {
 
+	private int idSede;
+	private String direccionSede;
+	
+	public Sede() {
+		super();
+	}
+
+	public Sede(int idSede, String direccionSede) {
+		super();
+		this.idSede = idSede;
+		this.direccionSede = direccionSede;
+	}
+
+	public int getIdSede() {
+		return idSede;
+	}
+
+	public void setIdSede(int idSede) {
+		this.idSede = idSede;
+	}
+
+	public String getDireccionSede() {
+		return direccionSede;
+	}
+
+	public void setDireccionSede(String direccionSede) {
+		this.direccionSede = direccionSede;
+	}
 }
