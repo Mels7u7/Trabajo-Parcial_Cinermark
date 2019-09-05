4295e0be75f15ffb3fbb340e2856fea9c5afdc7a
Propietario <Propietario@LAPTOP-F6J01QJQ>
2019-09-05 15:03:00 -0500

    Implementación de la entidad Herramienta

--- TrabajoParcial/src/main/java/pe/edu/upc/entity/Herramienta.java
+++ TrabajoParcial/src/main/java/pe/edu/upc/entity/Herramienta.java
@@ -2,4 +2,52 @@ package pe.edu.upc.entity;
 
 public class Herramienta {
 
+	private int idHerramienta;
+	private String nombreHerramienta;
+	private int stockHerramienta;
+	private String unidadHerramienta;
+	
+	public Herramienta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Herramienta(int idHerramienta, String nombreHerramienta, int stockHerramienta, String unidadHerramienta) {
+		this.idHerramienta = idHerramienta;
+		this.nombreHerramienta = nombreHerramienta;
+		this.stockHerramienta = stockHerramienta;
+		this.unidadHerramienta = unidadHerramienta;
+	}
+
+	public int getIdHerramienta() {
+		return idHerramienta;
+	}
+
+	public void setIdHerramienta(int idHerramienta) {
+		this.idHerramienta = idHerramienta;
+	}
+
+	public String getNombreHerramienta() {
+		return nombreHerramienta;
+	}
+
+	public void setNombreHerramienta(String nombreHerramienta) {
+		this.nombreHerramienta = nombreHerramienta;
+	}
+
+	public int getStockHerramienta() {
+		return stockHerramienta;
+	}
+
+	public void setStockHerramienta(int stockHerramienta) {
+		this.stockHerramienta = stockHerramienta;
+	}
+
+	public String getUnidadHerramienta() {
+		return unidadHerramienta;
+	}
+
+	public void setUnidadHerramienta(String unidadHerramienta) {
+		this.unidadHerramienta = unidadHerramienta;
+	}
 }
