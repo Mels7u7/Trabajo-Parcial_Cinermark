1c652178e24a2cc4e991833300c16a8d25fc810a
porta <porta@10.142.71.132>
2019-09-25 00:03:04 -0500

    Auditoria ya registra bota tu gaaaaaaaaaaa

---
+++ TrabajoParcial/src/main/java/pe/edu/upc/controller/DetalleAuditoriaController.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class DetalleAuditoriaController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
