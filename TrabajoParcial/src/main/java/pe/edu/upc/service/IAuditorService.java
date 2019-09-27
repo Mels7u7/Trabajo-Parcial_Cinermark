f5e50ac5bfe6b5fc2c876ec06b6b9c4a3ba5411a
sbast <sbast@192.168.1.136>
2019-09-27 08:55:08 -0500

    intento de busqueda

--- TrabajoParcial/src/main/java/pe/edu/upc/service/IAuditorService.java
+++ TrabajoParcial/src/main/java/pe/edu/upc/service/IAuditorService.java
@@ -1,9 +1,9 @@
 package pe.edu.upc.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.edu.upc.entity.Auditor;
-
 public interface IAuditorService {
 
 	public void insertar(Auditor auditor);
@@ -13,5 +13,13 @@ public interface IAuditorService {
 	public void modificar(Auditor auditor);
 
 	public List<Auditor> listar();
+
+	List<Auditor> getAll() throws Exception;
+
+	Optional<Auditor> getOne(Auditor aud) throws Exception;
+
+	List<Auditor> findAuditorByName(String name) throws Exception;
+
+	Optional<Auditor> findAuditorByDni(String dni) throws Exception;
 
 }
